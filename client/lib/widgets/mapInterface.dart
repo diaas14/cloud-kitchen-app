51e093d0c02e5470d69898859fe48faa49d03a00
Saipriya M N <priyasrinand@gmail.com>
2023-05-23 20:40:52 +0530

    Add a google maps interface to client app

--- client/lib/widgets/mapInterface.dart
+++ client/lib/widgets/mapInterface.dart
@@ -1,66 +1,70 @@
+import 'package:client/services/location_service.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:geocoding/geocoding.dart';
-import 'package:client/services/location_service.dart';
+import 'package:geocoding_platform_interface/src/models/placemark.dart';
+import 'package:geolocator_platform_interface/src/models/position.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
+import 'dart:async';
 
 class MapInterface extends StatefulWidget {
   const MapInterface({super.key});
 
   @override
-  State<MapInterface> createState() => _MapInterfaceState();
+  State<MapInterface> createState() => MapInterfaceState();
 }
 
-class _MapInterfaceState extends State<MapInterface> {
-  late Future<Placemark> _currentPlace;
+class MapInterfaceState extends State<MapInterface> {
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
 
-  @override
-  void initState() {
-    super.initState();
-    _currentPlace = determineCurrentPlace();
-  }
+  Future<Position> currentLocation = fetchPosition();
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<Placemark>(
-        future: _currentPlace,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            Placemark place = snapshot.data!;
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Your current location',
-                    style: TextStyle(
-                        letterSpacing: 1.2,
-                        color: Theme.of(context).primaryColor,
-                        fontWeight: FontWeight.bold)),
-                SizedBox(
-                  height: 20,
-                ),
-                Center(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.location_on,
-                        color: Theme.of(context).primaryColor,
-                      ),
-                      Text(
-                          '${place.street}, ${place.subLocality}, ${place.locality}, ${place.country}',
-                          style: TextStyle(
-                              letterSpacing: 1.2,
-                              color: Theme.of(context).primaryColor,
-                              fontWeight: FontWeight.bold)),
-                    ],
+    return Scaffold(
+      body: FutureBuilder<Position>(
+          future: currentLocation,
+          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+            if (snapshot.hasData) {
+              Position position = snapshot.data;
+              return GoogleMap(
+                mapType: MapType.normal,
+                initialCameraPosition: CameraPosition(
+                    target: LatLng(position.latitude, position.longitude),
+                    zoom: 15.0),
+                mapToolbarEnabled: false,
+                myLocationEnabled: false,
+                myLocationButtonEnabled: true,
+                compassEnabled: true,
+                rotateGesturesEnabled: true,
+                tiltGesturesEnabled: true,
+                zoomGesturesEnabled: true,
+                onMapCreated: (GoogleMapController controller) {
+                  _controller.complete(controller);
+                },
+                markers: <Marker>{
+                  Marker(
+                    markerId: MarkerId('currentLocationMarker'),
+                    position: LatLng(position.latitude, position.longitude),
                   ),
-                ),
-              ],
-            );
-          } else if (snapshot.hasError) {
-            return Text(snapshot.error.toString());
-          } else {
-            return Center(child: CircularProgressIndicator());
-          }
-        });
+                },
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text('${snapshot.error}'),
+              );
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          }),
+    );
   }
+}
+
+Future<Position> fetchPosition() async {
+  return await determinePosition();
 }
