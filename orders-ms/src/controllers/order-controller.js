3a24d844b555d9bb0ce2fd31ae4ac5da33d3651c
diaas14 <diya.diyu14@gmail.com>
2023-06-05 01:48:57 +0530

    Add fetch orders business logic

--- orders-ms/src/controllers/order-controller.js
+++ orders-ms/src/controllers/order-controller.js
@@ -3,10 +3,18 @@ const axios = require("axios");
 
 class OrderController {
   async createOrder(req, res) {
-    const { customerId, providerId, cartItems } = req.body;
+    const {
+      customerId,
+      providerId,
+      cartItems,
+      price,
+      userData,
+      providerDetails,
+    } = req.body;
     try {
       const parsedCartItems = JSON.parse(cartItems);
-      console.log(cartItems);
+      const parsedCustomerData = JSON.parse(userData);
+      const parsedProviderDetails = JSON.parse(providerDetails);
       const result = await axios.put(
         "http://business-profile-serv:4001/api/business-profile/transactions/",
         {
@@ -32,13 +40,71 @@ class OrderController {
         customerId: customerId,
         providerId: providerId,
         items: Object.values(parsedCartItems),
+        price: parseFloat(price),
+        customerData: parsedCustomerData,
+        providerDetails: parsedProviderDetails,
       };
 
       await newOrderRef.set(newOrder);
       console.log("Order created successfully.");
       return res.status(201).json({ message: "Order created successfully." });
     } catch (error) {
-      console.error("Error creating order:", error.message);
+      console.error("Error creating order:", error);
+      return res.status(500).json({ message: "Internal server error." });
+    }
+  }
+
+  async fetchOrdersOfUser(req, res) {
+    const userId = req.params.userId;
+
+    try {
+      const ordersRef = admin.firestore().collection("orders");
+      const querySnapshot = await ordersRef
+        .where("customerId", "==", userId)
+        .get();
+
+      if (querySnapshot.empty) {
+        return res
+          .status(404)
+          .json({ message: "No orders found for the user." });
+      }
+
+      const orders = [];
+      querySnapshot.forEach((doc) => {
+        const orderData = doc.data();
+        orders.push(orderData);
+      });
+
+      return res.status(200).json(orders);
+    } catch (error) {
+      console.error("Error fetching orders:", error);
+      return res.status(500).json({ message: "Internal server error." });
+    }
+  }
+
+  async fetchOrdersOfProvider(req, res) {
+    const providerId = req.params.providerId;
+    try {
+      const ordersRef = admin.firestore().collection("orders");
+      const querySnapshot = await ordersRef
+        .where("providerId", "==", providerId)
+        .get();
+
+      if (querySnapshot.empty) {
+        return res
+          .status(404)
+          .json({ message: "No orders found for the user." });
+      }
+
+      const orders = [];
+      querySnapshot.forEach((doc) => {
+        const orderData = doc.data();
+        orders.push(orderData);
+      });
+
+      return res.status(200).json(orders);
+    } catch (error) {
+      console.error("Error fetching orders:", error);
       return res.status(500).json({ message: "Internal server error." });
     }
   }
--- orders-ms/src/routes/order-route.js
+++ orders-ms/src/routes/order-route.js
@@ -7,4 +7,14 @@ const orderController = new OrderController();
 
 router.post("/", orderController.createOrder.bind(orderController));
 
+router.get(
+  "/user/:userId",
+  orderController.fetchOrdersOfUser.bind(orderController)
+);
+
+router.get(
+  "/provider/:providerId",
+  orderController.fetchOrdersOfProvider.bind(orderController)
+);
+
 module.exports = router;
